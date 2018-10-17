338b029bab5fe26158cf530dd97ff56fa6d49a73
shubhamsinha0001 <shubhamsinha1jan@gmail.com>
2018-10-17 22:39:25 +0530

    guvi programs shubham

---
+++ Beginner/Set-1/positive.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin>>a;
+    if(a>0)
+    {
+        cout<<"Positive";
+    }
+    else if(a==0)
+    {
+        cout<<"Zero";
+    }
+    else{
+        cout<<"Negative";
+    }
+}
